d551db8498586a3f7eb5c5d866a0a881905cfdc5
rhea-manuel <rheamanuel02@gmail.com>
2020-08-27 20:16:45 -0400

    MVP Finished

--- js/index.js
+++ js/index.js
@@ -1,28 +1,30 @@
-// JS goes here
-var modal = document.getElementById("overlay");
+// // JS goes here
+// var modal = document.getElementById("overlay");
 
-// Get the button that opens the modal
-var btn = document.getElementById("hamburger");
+// // Get the button that opens the modal
+// var btn = document.getElementById("hamburger");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+// // Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("close")[0];
 
-document.getElementById('hamburger').onclick = function(){
+// document.getElementById('hamburger').onclick = function(){
     
-}
+// }
 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
+// btn.onclick = function() {
+//   modal.style.display = "block";
+// }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// // }
+
+// element.addEventListener

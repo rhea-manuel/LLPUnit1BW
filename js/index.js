4c31e3841cc93b0ca9625b063b07be6167f977f6
rhea-manuel <rheamanuel02@gmail.com>
2020-08-27 20:11:50 -0400

    Finished MVP

--- js/index.js
+++ js/index.js
@@ -1,1 +1,28 @@
 // JS goes here
+var modal = document.getElementById("overlay");
+
+// Get the button that opens the modal
+var btn = document.getElementById("hamburger");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+document.getElementById('hamburger').onclick = function(){
+    
+}
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}

c5f0b5da79e215857b67274bccb3fb2ad921fac7
rhea-manuel <rheamanuel02@gmail.com>
2020-08-27 21:35:09 -0400

    Finished JS stretch goal

--- js/index.js
+++ js/index.js
@@ -1,30 +1,58 @@
-// // JS goes here
+// // // JS goes here
+var menuIcon = document.getElementById("hamburger");
+
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+//   document.getElementById("myNav").style.display = "block";
+  menuIcon.style.display="none";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+//   document.getElementById("myNav").style.display = "none";
+  menuIcon.style.display="block";
+}
 // var modal = document.getElementById("overlay");
 
 // // Get the button that opens the modal
-// var btn = document.getElementById("hamburger");
+// const btn = document.getElementById("hamburger");
 
 // // Get the <span> element that closes the modal
-// var span = document.getElementsByClassName("close")[0];
+// const close = document.getElementsByClassName("close")[0];
 
-// document.getElementById('hamburger').onclick = function(){
-    
-// }
 
-// btn.onclick = function() {
-//   modal.style.display = "block";
-// }
+// btn.addEventListener("click", ()=>{
+//     modal.style.display="block";
+// });
 
-// // When the user clicks on <span> (x), close the modal
-// span.onclick = function() {
-//   modal.style.display = "none";
-// }
+// close.addEventListener("click",()=>{
+//     modal.style.display="none";
+// });
 
-// // When the user clicks anywhere outside of the modal, close it
-// window.onclick = function(event) {
-//   if (event.target == modal) {
-//     modal.style.display = "none";
-//   }
+// // function showElement(element){
+// //     element.style.display="block";
 // // }
 
-// element.addEventListener
+// // function hideElement(){
+// //     element.style.display="none";
+// // }
+
+// // window.addEventListener("click", ()=>{
+// // });
+
+// // btn.onclick = function() {
+// //   modal.style.display = "block";
+// // }
+
+// // // When the user clicks on <span> (x), close the modal
+// // span.onclick = function() {
+// //   modal.style.display = "none";
+// // }
+
+// // // When the user clicks anywhere outside of the modal, close it
+// // window.onclick = function(event) {
+// //   if (event.target == modal) {
+// //     modal.style.display = "none";
+// //   }
+// // // }
